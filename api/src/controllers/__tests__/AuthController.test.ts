3697d0c740700eb4ccb0bb2670c21a3be1b3d079
Chris Guy <christien.guy@gmail.com>
2019-10-13 15:32:13 +0100

    refactor to export app ready for api endpoint testing. Write 'integration' tests for AuthController

--- api/src/controllers/__tests__/AuthController.test.ts
+++ api/src/controllers/__tests__/AuthController.test.ts
@@ -1,34 +1,58 @@
 /*eslint-env jest */
-import {
-  createConnection,
-  getConnection,
-  Entity,
-  getRepository
-} from "typeorm";
-import { User } from "../../entity/User";
+
 import AuthController from "../AuthController";
+import { createConnection, getConnection, getRepository } from "typeorm";
+import { User } from "../../entity/User";
+import { Question } from "../../entity/Question";
+import { Answer } from "../../entity/Answer";
+import { Vote } from "../../entity/Vote";
 
 let authController;
-
-beforeEach(() => {
-  authController = new AuthController();
-
-  return createConnection({
-    type: "postgres",
+let userRepository;
+beforeEach(async () => {
+  await createConnection({
+    type: "sqlite",
     database: ":memory:",
     dropSchema: true,
-    entities: [User],
+    entities: [User, Question, Answer, Vote],
     synchronize: true,
     logging: false
   });
+  authController = new AuthController();
+  userRepository = getRepository(User);
 });
 
-afterEach(() => {
-  // const conn = getConnection();
-  // conn.close();
+afterEach(async () => {
+  const conn = getConnection();
+  return conn.close();
 });
 
-test("should login correctly", async () => {
-  const response = await authController.login();
-  console.log(response);
+test("should fail login if no user", async () => {
+  expect(authController.login("admin@test.com", "password")).rejects.toThrow(
+    "no matching user"
+  );
+});
+
+test("should login if user exists", async () => {
+  // This currently mimics the usercontroller add user method
+  // This is a bit fragile since we need to remember to update this
+  // every time we update the user controller/auth methods
+  // TODO: look into using the controller directly here/moving that logic into
+  // a shared service
+  const mockCookie = jest.fn();
+  const user = await userRepository.create({
+    email: "admin@example.com",
+    username: "admin",
+    password: "admin",
+    role: "admin"
+  });
+  user.hashPassword();
+  await userRepository.save(user);
+
+  const response = await authController.login("admin@example.com", "admin", {
+    cookie: mockCookie
+  });
+
+  expect(mockCookie).toHaveBeenCalledTimes(1);
+  expect(response).toBeUndefined();
 });
