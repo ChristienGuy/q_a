b22558a222d6d878e9c569f690efb874b6ed9c09
Chris Guy <christien.guy@gmail.com>
2020-02-03 14:49:17 +0000

    Add register mutation. Upgrade dependencies

--- api/src/resolvers/UserResolver.ts
+++ api/src/resolvers/UserResolver.ts
@@ -1,11 +1,12 @@
 import { Resolver, Query, Mutation, Arg, Ctx } from "type-graphql";
 import { User } from "../entity/User";
 import { Repository, getRepository } from "typeorm";
-import { AuthenticationError } from "apollo-server-express";
+import { AuthenticationError, UserInputError } from "apollo-server-express";
 import { Response, Request } from "express";
 import { getTokens } from "../get-tokens";
 import { getCookies } from "../get-auth-cookies";
 import config from "../config/config";
+import { validate } from "class-validator";
 
 @Resolver(User)
 export class UserResolver {
@@ -26,9 +27,14 @@ export class UserResolver {
     @Ctx() { res }: { res: Response; req: Request }
   ) {
     // TODO: catch failure
-    const user: User = await this.userRepository.findOneOrFail({
-      where: { email }
-    });
+    let user: User;
+    try {
+      user = await this.userRepository.findOneOrFail({
+        where: { email }
+      });
+    } catch {
+      throw new AuthenticationError("no use with those details");
+    }
 
     if (!user.checkIfUnencryptedPasswordIsValid(password)) {
       throw new AuthenticationError("incorrect");
@@ -44,10 +50,38 @@ export class UserResolver {
 
   @Mutation(returns => Boolean)
   async logout(@Ctx() { res }: { res: Response }): Promise<boolean> {
-    console.log("hello");
-
     res.clearCookie(config.cookieNames.access);
     res.clearCookie(config.cookieNames.refresh);
     return true;
   }
+
+  @Mutation(returns => User)
+  async register(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Arg("username") username: string
+  ): Promise<User> {
+    let user = new User();
+    user.email = email;
+    user.username = username;
+    user.password = password;
+    user.role = "user";
+
+    const errors = await validate(User);
+    if (errors.length > 0) {
+      throw new UserInputError("invalid data");
+    }
+
+    user.hashPassword();
+
+    try {
+      await this.userRepository.save(user);
+    } catch (e) {
+      throw new UserInputError("invalid");
+    }
+
+    return user;
+  }
+
+  // TODO: change password mutation
 }
